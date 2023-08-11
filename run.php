d926b3b4267b82570252efddb8776a800eb54558
Ben Orpwood [bgo0026] <bgo0026@arastudent.ac.nz>
2023-08-11 14:34:18 +1200

    html added

--- run.php
+++ run.php
@@ -1,15 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>election</title>
+    </head>
+    <body>
+        <?php
 
-include "controller.php";
+        include "controller.php";
 
-$jsonPath="json/elections.json";
-$jsonString = file_get_contents($jsonPath);
-$ElectionJSON = json_decode($jsonString, true);
+        $jsonPath="json/elections.json";
+        $jsonString = file_get_contents($jsonPath);
+        $ElectionJSON = json_decode($jsonString, true);
 
-$theController=new Controller;
+        $theController=new Controller;
 
-$theElection=$theController->setup($ElectionJSON);
+        $theElection=$theController->setup($ElectionJSON);
 
-echo $theElection->__toString();
+        echo $theElection->__toString();
 
-?>
+        ?>
+    </body>
+</html>
